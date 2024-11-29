0f036d1df40766e2d73afe9ea3bb32e2d075614b
Stygus <kuba.tokarczyk@onet.eu>
2024-11-29 02:01:45 +0100

    Dodanie metody laczenia

--- Sortowanie.cpp
+++ Sortowanie.cpp
@@ -14,3 +14,32 @@ void Sortowanie::print(int* tablica, int size) {
     cout << endl;
 }
 
+void Sortowanie::laczenie(int tablica[], int start, int srodek, int koniec) {
+    int i = start, j = srodek + 1, k = 0;
+    int* tabela_pomocniczna = new int[koniec - start + 1];
+
+    while (i <= srodek && j <= koniec) {
+        if (tablica[i] <= tablica[j]) {
+            tabela_pomocniczna[k++] = tablica[i++];
+        }
+        else {
+            tabela_pomocniczna[k++] = tablica[j++];
+        }
+    }
+
+    while (i <= srodek) {
+        tabela_pomocniczna[k++] = tablica[i++];
+    }
+
+    while (j <= koniec) {
+        tabela_pomocniczna[k++] = tablica[j++];
+    }
+
+    for (int i = 0; i < k; i++) {
+        tablica[start + i] = tabela_pomocniczna[i];
+    }
+
+    delete[] tabela_pomocniczna;
+}
+
+
