02ea1338373f9af0e8191e93ce707c9d1763ee69
Stygus <kuba.tokarczyk@onet.eu>
2024-11-29 02:01:06 +0100

    Pierwsze metody

--- Sortowanie.cpp
+++ Sortowanie.cpp
@@ -3,3 +3,14 @@
 
 using namespace std;
 
+Sortowanie::~Sortowanie() {
+    delete[] tablica;
+}
+
+void Sortowanie::print(int* tablica, int size) {
+    for (int i = 0; i < size; i++) {
+        cout << tablica[i] << " ";
+    }
+    cout << endl;
+}
+
